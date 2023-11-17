6da842bf8f434a80ad7b9127c061a112a7821452
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2023-11-17 21:04:52 +0530

    updated cors

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: `${process.env.ENDPOINT}`,
+    origin: `*`,
   },
 });
 
